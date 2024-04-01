c9d5669e38468d0cd771832ee022ff8b8813849a
swastik <swastik@kodiary.com>
2024-04-01 20:46:36 +0545

    inital edit

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        "./index.html",
+        "./src/**/*.{js,ts,jsx,tsx}",
+        "./node_modules/tw-elements/js/**/*.js"
+    ],
+    theme: {
+        extend: {
+            colors: {
+                primary: {
+                    DEFAULT: "#047857",
+                },
+                secondary: {
+                    DEFAULT: "#731DD8",
+                },
+                tertiary: {
+                    DEFAULT: "#A2E8DD"
+                }
+            },
+            fontFamily: {
+                // "roboto": ['Roboto', 'sans-serif'],
+                // 'mono':['Roboto Slab', 'serif']
+            },
+            screens: {
+                '3xl': '1880px',
+            },
+        },
+    },
+    // plugins: [require("tw-elements/plugin.cjs")],
+    darkMode: "class"
+}
