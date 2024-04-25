b97024c28b38180cbb23ebf51bd6d15c567bc762
swastik007 <swastik.nakarmi@gmail.com>
2024-04-25 16:58:24 +0545

    subjects icon

--- src/components/SubjectCard.jsx
+++ src/components/SubjectCard.jsx
@@ -5,22 +5,22 @@ const SubjectCard = ({ icon, title}) => {
     return(
         <motion.div                   
             whileHover={{
-            scale: 1.05,
-            rotate: 1,      
+            scale: 1.01,
+            rotate: -1,      
             ease: "easeOut", 
             onDurationChange: 2,
             borderRadius: 10,                                              
             }}
-            className="hover:text-slate-800 bg-gray-400/10 hover:bg-blue-500 group " >
+            className="hover:text-slate-800 bg-gray-400/10 hover:bg-gradient-to-r hover:from-cyan-500 hover:to-blue-500 group " >
             <div className="overflow-hidden backdrop-blur-sm rounded-lg shadow">
                 <div className="px-4 py-8">
                     <div className="flex items-center">
                         <div className="w-2/5 relative flex items-center justify-center mx-auto  group-hover:invert-0">
-                            <img alt="" className="w-12 h-12 object-contain group-hover:invert" src={icon} />
+                            <img alt="" className="w-12 h-12 object-contain group-hover:bg-white rounded-full" src={icon} />
                         </div>
 
                         <div className="ml-5 mr-auto w-3/5">
-                            <p className="text-lg font-semibold leading-tight">{title}</p>
+                            <p className="text-lg font-semibold leading-tight group-hover:text-white">{title}</p>
                         </div>
                     </div>
                 </div>
--- src/components/SubjectsSection.jsx
+++ src/components/SubjectsSection.jsx
@@ -7,16 +7,27 @@ const Subjects = () => {
             className="bg-sky-100 py-10 sm:py-16 lg:py-24 relative bg-cover"
          >
             <div className="max-w-2xl mx-auto text-center">
-            <h2 className="text-3xl font-bold leading-tight text-gray-200 sm:text-4xl lg:text-5xl">
-                Industry Expertise
+            <h2 className="text-3xl font-bold leading-tight text-stone-800 sm:text-4xl lg:text-5xl">
+                Subjects Covered
             </h2>
-            <p className="max-w-xl mx-auto mt-4 text-base leading-relaxed text-gray-300">
-                Gaining trust of hundreds of clients throughout the world, we have gained experience in a wide range of industries. Realizing the need of the digital world our clients regard us as their IT partners to take care of their digital needs.
+            <p className="max-w-xl mx-auto mt-4 text-base leading-relaxed text-stone-700">
+                Gaining trust of hundreds of student and parents throughout the world, we have gained experience in a wide range of industries. Realizing the need of the digital world our clients regard us as their IT partners to take care of their digital needs.
             </p>
             </div>
             <div className="container max-w-3xl mx-auto grid grid-cols-1 gap-6 mt-12 lg:mt-16 xl:gap-10 sm:grid-cols-2 lg:grid-cols-4 p-8 lg:p-0">
                 <SubjectCard title="Mathematics" icon="images/subjects/maths.png" />
                 <SubjectCard title="Science" icon="images/subjects/science.png" />
+                <SubjectCard title="Science" icon="images/subjects/business.png" />
+                <SubjectCard title="Science" icon="images/subjects/chemistry.png" />
+                <SubjectCard title="Science" icon="images/subjects/environment.png" />
+                <SubjectCard title="Science" icon="images/subjects/geography.png" />
+                <SubjectCard title="Science" icon="images/subjects/history.png" />
+                <SubjectCard title="Science" icon="images/subjects/medicine.png" />
+                <SubjectCard title="Science" icon="images/subjects/languages.png" />
+                <SubjectCard title="Science" icon="images/subjects/english.png" />
+                <SubjectCard title="Science" icon="images/subjects/german.png" />
+                <SubjectCard title="Science" icon="images/subjects/science.png" />
+                
             </div>
          </section>
     )
