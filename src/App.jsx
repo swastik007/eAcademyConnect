728673c02668ebbe0abfb14fe80491c99c2add79
nistha99 <wanderer.april@gmail.com>
2024-04-28 13:58:15 +0545

    test commit

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import Subjects from './components/SubjectsSection.jsx';
 import Testimonials from './components/TestimonialSection.jsx';
 import BannerSection from './components/BannerSection.jsx';
 import { InfiniteMovingCardsDemo } from './components/TestimonialSlider.jsx';
+/* test comment */
 function App() {
   const [count, setCount] = useState(0)
 
